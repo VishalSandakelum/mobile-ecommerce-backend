1ea88f6f2c867d83fe3a50466caadbbf9bb71440
vishalgit2005 <vishalsandakelum20@gmail.com>
2025-03-02 13:42:09 +0530

    project setup

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const app = express();
+const mongo_uri = process.env.MONGO_URI;
+const PORT = process.env.PORT;
+
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+app.use(cors());
+
+//Routes
+
+mongoose
+  .connect(mongo_uri)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("DB connection error : ", err);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
